81b1c3e8f74d434e2cf7a49af9ce769fa64505e2
SK <sudhirid@gmail.com>
2020-08-13 23:13:16 -0500

    First commit for Discovery Service

---
+++ KmsDiscoveryService/src/test/java/com/skumar/kms/discovery/KmsDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skumar.kms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmsDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
