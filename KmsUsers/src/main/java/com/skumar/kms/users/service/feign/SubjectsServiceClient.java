5437ba33ff8008a8967c261a28b108277b5844dc
SK <sudhirid@gmail.com>
2020-08-23 21:33:30 -0500

    Section 20: Error Handling with Feign Hystrix FallbackFactory
    Feign Hystrix FallbackFactory

--- KmsUsers/src/main/java/com/skumar/kms/users/service/feign/SubjectsServiceClient.java
+++ KmsUsers/src/main/java/com/skumar/kms/users/service/feign/SubjectsServiceClient.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.security.auth.Subject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.skumar.kms.users.model.SubjectResponseModel;
 
-@FeignClient(name="kms-subjects", fallback = SubjectFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name="kms-subjects", fallbackFactory = SubjectFallbackFatcory.class)
 public interface SubjectsServiceClient {
 	
 	//@GetMapping("/kms/subjects/by-user/{userId}")
@@ -22,12 +27,36 @@ public interface SubjectsServiceClient {
 }
 
 @Component
-class SubjectFallback implements SubjectsServiceClient {
+class SubjectFallbackFatcory implements FallbackFactory<SubjectsServiceClient> {
 
 	@Override
-	public List<SubjectResponseModel> getSubjectsByUser(String userId) {
+	public SubjectsServiceClient create(Throwable cause) {
 		// TODO Auto-generated method stub
-		return new ArrayList<>();
+		return new SubjectsServiceClientFallback(cause);
 	}
 
+}
+
+class SubjectsServiceClientFallback implements SubjectsServiceClient {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private final Throwable cause;
+
+	public SubjectsServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
+
+	
+	@Override
+	public List<SubjectResponseModel> getSubjectsByUser(String userId) {
+		if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getSubjectsByUser was called with userId: " + userId + ". Error message: "
+					+ cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: " + cause.getLocalizedMessage());
+		}
+		return new ArrayList<>();
+	}
+	
 }
